6d37fbbaa77f858a7848b0a20a768f5d1c7364bb
Jun Le <tayjunle@gmail.com>
2019-06-09 14:34:45 +0800

    add router

---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../components/Home.vue'
+import Trips from '../components/Trips.vue'
+import Reminders from '../components/Reminders.vue'
+import Profile from '../components/User/Profile.vue'
+import Login from '../components/User/Login.vue'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+    routes: [
+        {
+            path: '/',
+            name: 'Home',
+            component: Home
+        },
+        { 
+            path: '/trips', 
+            name: 'Trips', 
+            component: Trips 
+        },
+        { 
+            path: '/reminders', 
+            name: 'Reminders', 
+            component: Reminders 
+        },
+        { 
+            path: '/profile', 
+            name: 'Profile', 
+            component: Profile 
+        },
+        { 
+            path: '/login', 
+            name: 'Login', 
+            component: Login 
+        }
+    ],
+    mode: 'history'
+})
