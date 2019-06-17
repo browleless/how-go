3b4870ebe96f6130a3f616f47eb7552c0c0f7fba
Jun Le <tayjunle@gmail.com>
2019-06-18 01:52:22 +0800

    debug route instructions and reorganize trips array data

--- src/components/Trips/TripsInstruction.vue
+++ src/components/Trips/TripsInstruction.vue
@@ -1,19 +1,27 @@
 <template>
   <div>
-    <v-btn @click="show">Get routes</v-btn>
-    <v-btn v-if="!hidden && loaded" @click="hidden = !hidden">Show</v-btn>
-    <v-btn v-if="hidden && loaded" @click="hidden = !hidden">Hide</v-btn>
-    <div v-if="loaded && hidden">
-      <div v-for="trips in tripsInfo" :key="trips.index" id="individualTrip">
+    <v-btn v-if="loaded" @click="loaded = !loaded">Show</v-btn>
+    <v-btn v-if="!loaded" @click="loaded = !loaded">Hide</v-btn>
+    <div v-if="!loaded">
+      <div v-for="trips in tripsInfo" :key="trips.index" id="possibleTrips">
+        Route Options for Destination
         <div
-          v-for="fullInstructions in trips.fullInstructions"
-          :key="fullInstructions.index"
-          id="eachInstruction"
+          v-for="itinararies in trips.itinararies"
+          :key="itinararies.index"
+          id="specificTrip"
         >
+        <div>{{ itinararies.travelTime }}</div>
+        <div>{{ itinararies.cost }}</div>
           <ul
-            v-for="instructions in fullInstructions.steps"
+            v-for="instructions in itinararies.fullInstructions"
             :key="instructions.index"
-          >{{ instructions }}</ul>
+            id="instructions"
+          >
+            <div
+              v-for="steps in instructions.steps"
+              :key="steps.index"
+            >{{ steps }}</div>
+          </ul>
         </div>
       </div>
     </div>
@@ -26,49 +34,50 @@ export default {
   data() {
     return {
       tripsInfo: [],
-      routes: [],
       loaded: false,
-      hidden: true
     }
   },
   methods: {
-    show() {
+    async init() {
       for (var i = 0; i < this.$store.getters.calendarEvents.length - 1; i++) {
-        this.$http.get(
-            'https://developers.onemap.sg/privateapi/routingsvc/route?start=' + this.$store.getters.calendarEvents[i].latlng +'&end=' + this.$store.getters.calendarEvents[i + 1].latlng +'&routeType=pt&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjI3MjksInVzZXJfaWQiOjI3MjksImVtYWlsIjoiMTk5OG5hdWZhbEBnbWFpbC5jb20iLCJmb3JldmVyIjpmYWxzZSwiaXNzIjoiaHR0cDpcL1wvb20yLmRmZS5vbmVtYXAuc2dcL2FwaVwvdjJcL3VzZXJcL3Nlc3Npb24iLCJpYXQiOjE1NjA0MDk3MjIsImV4cCI6MTU2MDg0MTcyMiwibmJmIjoxNTYwNDA5NzIyLCJqdGkiOiI0NjUzN2YzZmE4YjEyZTMwZTMzODdlZDZmMzgwZTEzMiJ9.0WQUOPl_s0n9TxNaLondpmVamvAvt0PLFSYzCQlkhpU&date=' + this.$store.getters.calendarEvents[i].date + '&time=10:30:00&mode=TRANSIT&numItineraries=1'
+        var trip = {}
+        await this.$http.get(
+            'https://developers.onemap.sg/privateapi/routingsvc/route?start=' + this.$store.getters.calendarEvents[i].latlng +'&end=' + this.$store.getters.calendarEvents[i + 1].latlng +'&routeType=pt&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjI3MjksInVzZXJfaWQiOjI3MjksImVtYWlsIjoiMTk5OG5hdWZhbEBnbWFpbC5jb20iLCJmb3JldmVyIjpmYWxzZSwiaXNzIjoiaHR0cDpcL1wvb20yLmRmZS5vbmVtYXAuc2dcL2FwaVwvdjJcL3VzZXJcL3Nlc3Npb24iLCJpYXQiOjE1NjA0MDk3MjIsImV4cCI6MTU2MDg0MTcyMiwibmJmIjoxNTYwNDA5NzIyLCJqdGkiOiI0NjUzN2YzZmE4YjEyZTMwZTMzODdlZDZmMzgwZTEzMiJ9.0WQUOPl_s0n9TxNaLondpmVamvAvt0PLFSYzCQlkhpU&date=' + this.$store.getters.calendarEvents[i].date + '&time=10:30:00&mode=TRANSIT&numItineraries=3'
           )
           .then(res => {
+            var routes = []
             for (var j = 0; j < res.data.plan.itineraries.length; j++) {
-              this.routes.push(res.data.plan.itineraries[j])
+              routes.push(res.data.plan.itineraries[j])
             }
             // route instructions
-            for (var k = 0; k < this.routes.length; k++) {
+            var itineraries = []
+            for (var k = 0; k < routes.length; k++) {
               var tripInfo = {}
               tripInfo["travelTime"] =
                 "Approx. travel time: " +
-                Math.round(this.routes[k].duration / 60) +
+                Math.round(routes[k].duration / 60) +
                 " min"
-              tripInfo["cost"] = "Cost: $" + this.routes[k].fare
+              tripInfo["cost"] = "Cost: $" + routes[k].fare
               var fullInstructions = []
-              for (var l = 0; l < this.routes[k].legs.length; l++) {
+              for (var l = 0; l < routes[k].legs.length; l++) {
                 var instructions = []
                 var text = ""
                 var steps = {}
-                // if (l === 0) {
-                //   this.routes[k].legs[l].from.name = this.$store.getters.calendarEvents[i].name
-                // }
-                // if (l === this.routes[k].legs.length - 1) {
-                //   this.routes[k].legs[l].to.name = this.$store.getters.calendarEvents[i + 1].name
-                // }
-                instructions.push(this.routes[k].legs[l].from.name);
-                if (this.routes[k].legs[l].mode === "WALK") {
+                if (l === 0) {
+                  routes[k].legs[l].from.name = this.$store.getters.calendarEvents[i].name
+                }
+                if (l === routes[k].legs.length - 1) {
+                  routes[k].legs[l].to.name = this.$store.getters.calendarEvents[i + 1].name
+                }
+                instructions.push(routes[k].legs[l].from.name);
+                if (routes[k].legs[l].mode === "WALK") {
                   text =
                     "Walk for " +
-                    Math.round(this.routes[k].legs[l].distance) +
+                    Math.round(routes[k].legs[l].distance) +
                     " m to " +
-                    this.routes[k].legs[l].to.name
-                  if (this.routes[k].legs[l].to.stopId !== undefined) {
-                    var id = this.routes[k].legs[l].to.stopId.slice(6);
+                    routes[k].legs[l].to.name
+                  if (routes[k].legs[l].to.stopId !== undefined) {
+                    var id = routes[k].legs[l].to.stopId.slice(6);
                     if (id.length >= 5) {
                       text += " (Bus Stop: " + id + ")"
                     } else {
@@ -77,16 +86,16 @@ export default {
                   }
                   instructions.push(text)
                   instructions.push(
-                    "(" + Math.round(this.routes[k].legs[l].distance) + " m)"
+                    "(" + Math.round(routes[k].legs[l].distance) + " m)"
                   )
                 } else {
                   text =
                     "Take " +
-                    this.routes[k].legs[l].routeId +
+                    routes[k].legs[l].routeId +
                     " and alight at " +
-                    this.routes[k].legs[l].to.name +
+                    routes[k].legs[l].to.name +
                     " "
-                  var id2 = this.routes[k].legs[l].to.stopId.slice(6);
+                  var id2 = routes[k].legs[l].to.stopId.slice(6);
                   if (id2.length >= 5) {
                     text += "(Bus Stop: " + id2 + ")"
                   } else {
@@ -94,11 +103,11 @@ export default {
                   }
                   instructions.push(text);
                   instructions.push(
-                    Math.round(this.routes[k].legs[l].duration / 60) +
+                    Math.round(routes[k].legs[l].duration / 60) +
                       " min(" +
-                      (this.routes[k].legs[l].intermediateStops.length + 1) +
+                      (routes[k].legs[l].intermediateStops.length + 1) +
                       " stops, " +
-                      Math.round((this.routes[k].legs[l].distance / 1000) * 10) /
+                      Math.round((routes[k].legs[l].distance / 1000) * 10) /
                         10 +
                       " km)"
                   )
@@ -107,25 +116,34 @@ export default {
                 fullInstructions.push(steps)
                 tripInfo["fullInstructions"] = fullInstructions
               }
-              this.tripsInfo.push(tripInfo)
+              itineraries.push(tripInfo)
+              trip['itinararies'] = itineraries
             }
           })
           .catch(err => {
             console.log(err)
           })
+          this.tripsInfo.push(trip)
       }
-      this.loaded = true;
     }
+  },
+  mounted() {
+    this.init()
+    this.loaded = true
   }
 }
 </script>
 
 <style scoped>
-#individualTrip {
+#possibleTrips {
   border: 7px solid red;
   margin-bottom: 30px;
 }
-#eachInstruction {
-  border: 2px solid blue;
+#specificTrip {
+  border: 5px solid blue;
+}
+#instructions {
+  border: 3px solid green;
+  margin: 20px 0;
 }
 </style>
