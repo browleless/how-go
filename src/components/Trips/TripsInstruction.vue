67a5bcc74b05507e68d3979a5d2c93faf06f8727
Jun Le <tayjunle@gmail.com>
2019-07-10 14:42:19 +0800

    add routes sorting choice and current location option

--- src/components/Trips/TripsInstruction.vue
+++ src/components/Trips/TripsInstruction.vue
@@ -3,21 +3,50 @@
     <v-layout row>
       <v-flex>
         <v-card>
-          <v-toolbar color="light-blue lighten-2">
+          <v-toolbar extended extension-height="40px" color="light-blue lighten-2">
             <v-icon>directions</v-icon>
             <v-toolbar-title justify-space-around>
               <v-card-text class="title">Route Options</v-card-text>
             </v-toolbar-title>
+            <template v-slot:extension>
+              <v-radio-group class="mt-2" v-model="sortBy" row>
+                <div class="pr-1">Sort By:</div>
+                <v-radio 
+                  color="black" 
+                  label="Fastest" 
+                  value="fastest"
+                ></v-radio>
+                <v-radio 
+                  color="black" 
+                  label="Cheapest" 
+                  value="cheapest"
+                ></v-radio>
+              </v-radio-group>
+            </template>
             <v-spacer></v-spacer>
+            <v-btn @click="init(currId)" icon>
+              <v-icon>refresh</v-icon>
+            </v-btn>
+            <v-btn @click="updateLocation" icon>
+              <v-icon>gps_fixed</v-icon>
+            </v-btn>
+            <v-btn to="/trips" icon>
+              <v-icon>arrow_back</v-icon>
+            </v-btn>
           </v-toolbar>
           <v-expansion-panel>
             <v-expansion-panel-content
-              v-for="trip in itineraries"
+              v-for="trip in sortedItineraries"
               :key="trip.index"
               @click.native="$emit('render-polyline', trip.polyline)"
             >
               <template v-slot:header>
-                {{trip.travelTime}} {{trip.cost}}
+                <div v-if="trip.travelTime < 3600">
+                  Approx. travel time: {{ Math.floor(trip.travelTime / 60) }} min Cost: ${{ trip.cost }}
+                </div>
+                <div v-if="trip.travelTime >= 3600">
+                  Approx. travel time: {{ Math.floor(trip.travelTime / 3600) }} hr {{ Math.floor((trip.travelTime - Math.floor(trip.travelTime / 3600) * 3600) / 60) }} min Cost: ${{ trip.cost }}
+                </div>
               </template>
               <v-timeline dense light align-top>
                 <v-timeline-item
@@ -47,12 +76,29 @@ export default {
     return {
       loaded: false,
       itineraries: [],
+      currId: null,
+      currEvent: null,
+      nextEvent: null,
+      sortBy: 'fastest'
+    }
+  },
+  computed: {
+    sortedItineraries() {
+      if (this.sortBy === 'fastest') {
+        return this.itineraries.slice().sort((x, y) => x.travelTime - y.travelTime)
+      } else {
+        return this.itineraries.slice().sort((x, y) => x.cost - y.cost)
+      }
     }
   },
   methods: {
     async init(id) {
-      const currEvent = this.$store.getters.calendarEvents[id]
-      const nextEvent = this.$store.getters.calendarEvents[id + 1]
+      this.currId = id
+      this.itineraries = []
+      if (this.currEvent === null) {
+        this.currEvent = this.$store.getters.calendarEvents[id]
+      }
+      this.nextEvent = this.$store.getters.calendarEvents[id + 1]
       let oneMapApiKey = ''
       var bodyFormData = new FormData()
       bodyFormData.append('email', 'howgodev@gmail.com')
@@ -63,24 +109,24 @@ export default {
         config: { headers: { 'Content-Type': 'multipart/form-data' } },
         data: bodyFormData
       })
-        .then(res => {
-          oneMapApiKey = res.data.access_token
-        })
-        .catch(err => {
-          console.log(err)
-        })
+      .then(res => {
+        oneMapApiKey = res.data.access_token
+      })
+      .catch(err => {
+        console.log(err)
+      })
       await this.$http
         .get(
           'https://developers.onemap.sg/privateapi/routingsvc/route?start=' +
-            currEvent.latlng +
+            this.currEvent.latlng +
             '&end=' +
-            nextEvent.latlng +
+            this.nextEvent.latlng +
             '&routeType=pt&token=' +
             oneMapApiKey +
             '&date=' +
-            currEvent.date +
+            this.currEvent.date +
             '&time=' +
-            currEvent.startTime +
+            this.currEvent.startTime +
             '&mode=TRANSIT&numItineraries=3'
         )
         .then(res => {
@@ -95,11 +141,12 @@ export default {
             var tripInfo = {}
 
             // route summary
-            tripInfo['travelTime'] =
-              'Approx. travel time: ' +
-              Math.round(routes[k].duration / 60) +
-              ' min'
-            tripInfo['cost'] = 'Cost: $' + routes[k].fare
+            tripInfo['travelTime'] = routes[k].duration
+            if (Number(routes[k].fare)) {
+              tripInfo['cost'] = Number(routes[k].fare)
+            } else {
+              tripInfo['cost'] = routes[k].fare
+            }
             var fullInstructions = []
             var polylineLatLng = []
             for (var l = 0; l < routes[k].legs.length; l++) {
@@ -110,10 +157,10 @@ export default {
 
               //name
               if (l === 0) {
-                routes[k].legs[l].from.name = currEvent.name
+                routes[k].legs[l].from.name = this.currEvent.name
               }
               if (l === routes[k].legs.length - 1) {
-                routes[k].legs[l].to.name = nextEvent.name
+                routes[k].legs[l].to.name = this.nextEvent.name
               }
               instructions.push(routes[k].legs[l].from.name)
 
@@ -166,6 +213,53 @@ export default {
             this.itineraries.push(tripInfo)
           }
         })
+        .catch(() => {
+          alert('Onemap API error, please refresh')
+        })
+    },
+    updateLocation() {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        let oneMapApiKey = ''
+        var bodyFormData = new FormData()
+        bodyFormData.append('email', 'howgodev@gmail.com')
+        bodyFormData.append('password', 'vZG8qgeXpM')
+        await this.$http({
+            method: 'post',
+            url: 'https://developers.onemap.sg/privateapi/auth/post/getToken',
+            config: { headers: { 'Content-Type': 'multipart/form-data' } },
+            data: bodyFormData
+        })
+        .then(res => {
+            oneMapApiKey = res.data.access_token;
+        })
+        .catch(err => {
+            console.log(err);
+        })
+        this.$http.get('https://developers.onemap.sg/privateapi/commonsvc/revgeocode?location=' + position.coords.latitude + ',' + position.coords.longitude + '&token=' + oneMapApiKey + '&buffer=100')
+        .then(res => {
+          const d = new Date()
+          const dateToday = new Date(d.getTime() - (d.getTimezoneOffset() * 60000)).toISOString()
+          const currDate = dateToday.slice(0, 10)
+          const currTime = dateToday.slice(11, 19)
+          var eventInfo = {}
+          eventInfo['name'] = res.data.GeocodeInfo[0].BUILDINGNAME
+          eventInfo['latlng'] = res.data.GeocodeInfo[0].LATITUDE + ',' + res.data.GeocodeInfo[0].LONGITUDE
+          eventInfo['date'] = currDate
+          eventInfo['startTime'] = currTime
+          this.currEvent = eventInfo
+          this.init(this.currId)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+      }, () => {
+        alert('Position not found, please try again!')
+      }, 
+      { 
+        enableHighAccuracy: true, 
+        maximumAge: 0,
+        timeout: 10000
+      })
     }
   }
 }
